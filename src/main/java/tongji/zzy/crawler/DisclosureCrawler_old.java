ce23ea2540a2147bda26cc34dfd959e688657765
vanishedzhou <vanishedzhou@163.com>
2016-12-27 15:44:48 +0800

    'refactor'

---
+++ src/main/java/tongji/zzy/crawler/DisclosureCrawler_old.java
@@ -0,0 +1,103 @@
+package tongji.zzy.crawler;
+
+import org.slf4j.Logger;
+import tongji.zzy.handler.HandlerWithJson;
+import tongji.zzy.model.DisclosureInfo;
+import tongji.zzy.resource.DisclosureCategory;
+import tongji.zzy.resource.FCLog;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@SuppressWarnings("Duplicates")
+public class DisclosureCrawler_old implements PageProcessor {
+	private static final Logger logger = FCLog.getLogger(DisclosureCrawler_old.class);
+
+	private Site site = Site.me().setRetryTimes(5).setSleepTime(10000);
+	private static String currentDisclosureCategory = "";
+	private static String TARGET_CONTENT_URL = "http://data\\.eastmoney\\.com/notice/\\d+/\\w+.html";
+	private static String LIST_URL = "http://data\\.eastmoney\\.com/Notice/Noticelist\\.aspx\\?.*page=\\d+";
+
+	@Override
+	public Site getSite() {
+		return site;
+	}
+
+	@Override
+	public void process(Page page) {
+		if (page.getUrl().regex(LIST_URL).match()) {
+			System.out.println("This is the list page...");
+
+			// add content urls
+			List<String> targetContentUrls = page.getHtml()
+					.xpath("/html/body/div[@class='mainFrame']//table[@class='tableCont']/tbody").links()
+					.regex(TARGET_CONTENT_URL).all();
+			page.addTargetRequests(targetContentUrls);
+
+			// add list urls
+			List<String> listUrls = page.getHtml().xpath("/html/body/div[@class='mainFrame']//div[@class='PageNav']")
+					.links().regex(LIST_URL).all();
+			page.addTargetRequests(listUrls);
+		} else if (page.getUrl().regex(TARGET_CONTENT_URL).match()) {
+			System.out.println("This is the conent page...");
+
+			String date_raw = page.getHtml().xpath("html/body//div[@class='content']//h5/text()").toString();
+			Date date = Date.valueOf(date_raw.split("：")[1]);
+			String title = page.getHtml().xpath("html/body//div[@class='content']//h4/text()").toString();
+			String content = page.getHtml().xpath("html/body//div[@class='content']//pre/text()").toString();
+
+			// find companyName and companyCode from content text(first n
+			// characters) using regular expression
+			Pattern pattern = Pattern.compile(".*代码：(\\d+).*简称：(\\S+).*");
+			Matcher matcher = pattern.matcher(content.subSequence(0, Math.min(100, content.length())));
+			String companyCode = "";
+			String companyName = "";
+			if (matcher.find()) {
+				companyCode = matcher.group(1);
+				companyName = matcher.group(2);
+
+			} else {
+				System.out.println("companyCode and companyName not found in the content text.");
+			}
+
+			// create the DisclosureInfo entity
+			DisclosureInfo disclosureInfo = new DisclosureInfo();
+			disclosureInfo.setUrl(page.getUrl().toString());
+			disclosureInfo.setDisclosureCategory(currentDisclosureCategory);
+			disclosureInfo.setDate(date);
+			disclosureInfo.setTitle(title);
+			disclosureInfo.setContent(String.valueOf(content.length()));
+			disclosureInfo.setCompanyCode(companyCode);
+			disclosureInfo.setCompanyName(companyName);
+
+			// convert to JSon string
+			String jsonString = HandlerWithJson.convertJavaClassToJsonStream(disclosureInfo);
+
+		}
+	}
+
+	public static void main(String[] args) {
+		// Spider.create(new DisclosureCrawler())
+		// .addUrl("http://data.eastmoney.com/Notice/Noticelist.aspx?type=1&market=all&date=&page=1")
+		// .run();
+
+		// run different spider according to different disclosure categories
+		for (DisclosureCategory dc : DisclosureCategory.values()) {
+			currentDisclosureCategory = dc.name();
+			Spider.create(new DisclosureCrawler_old()).addUrl(getSpecifiedDisclosureCategoryUrl(dc.toString())).run();
+		}
+
+	}
+
+	// get different disclosure categories' starting url
+	private static String getSpecifiedDisclosureCategoryUrl(String categoryCode) {
+		return "http://data.eastmoney.com/Notice/Noticelist.aspx?page=&market=all&date=&type=" + categoryCode;
+	}
+
+}
---
+++ src/main/java/tongji/zzy/crawler/NewsCrawler_hexun.java
@@ -0,0 +1,82 @@
+package tongji.zzy.crawler;
+
+import org.slf4j.Logger;
+import tongji.zzy.handler.HandlerWithJson;
+import tongji.zzy.model.NewsInfo;
+import tongji.zzy.resource.FCLog;
+import tongji.zzy.transfer.ElasticSearchStorage;
+import tongji.zzy.utils.CrawlerDateUtils;
+import tongji.zzy.utils.CrawlerStringUtils;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+import java.util.Date;
+import java.util.List;
+
+@SuppressWarnings("Duplicates")
+public class NewsCrawler_hexun implements PageProcessor{
+	private static final Logger logger = FCLog.getLogger(NewsCrawler_hexun.class);
+	protected String listUrlPattern = "http://data\\.eastmoney\\.com/Notice/Noticelist\\.aspx\\?.*page=\\d+";
+	protected String contentUrlPattern = ".*/[0-9-]+/[0-9]+\\.html";
+
+	private Site site = Site.me().setRetryTimes(5).setSleepTime(10000);
+
+	public static void main(String[] args) {
+		Spider.create(new NewsCrawler_hexun()).addUrl("http://futures.hexun.com/")
+				// .addPipeline(new ConsolePipeline())
+				// .addPipeline(new FilePipeline("result"))
+				.thread(1)
+				.run();
+	}
+
+	@Override
+	public void process(Page page) {
+		//和讯期货-今日头条
+		if(page.getUrl().regex(contentUrlPattern).match()) {
+			System.out.println("this is a content page...");
+			String titleXPath = "/html/body//div[@class='layout mg articleName']/h1/text()";
+			String dateXPath = "/html/body//div[@class='layout mg articleName']//span[@class='pr20']/text()";
+			String infoSourceXPath = "/html/body//div[@class='layout mg articleName']//div[@class='clearfix']/div/a/text()";
+			String contentXPath = "/html/body//div[@class='layout mg clearfix']//div[@class='art_context']/div[@class='art_contextBox']";
+
+			String title = page.getHtml().xpath(titleXPath).toString();
+			String dateStr = page.getHtml().xpath(dateXPath).toString();
+			Date date = CrawlerDateUtils.parseStringToDate(dateStr);
+			String infoSource = page.getHtml().xpath(infoSourceXPath).toString();
+			String content = page.getHtml().xpath(contentXPath).toString();
+			content = CrawlerStringUtils.removeHtmlElements(content);
+
+			NewsInfo newsInfo = new NewsInfo();
+			newsInfo.setUrl(page.getUrl().toString());
+			newsInfo.setTitle(title);
+			newsInfo.setDate(date);
+			newsInfo.setInfoSource(infoSource);
+			newsInfo.setContent(content);
+			//set typeNewsSource and document
+			newsInfo.typeNewsSource = "hexunFutures";
+
+			//get newsinfo entity
+			String jsonString = HandlerWithJson.convertJavaClassToJsonStream(newsInfo);
+			logger.info("newsinfo json string: "+jsonString);
+//			System.out.println(jsonString);
+
+			//store data to es
+			ElasticSearchStorage.esStorage(newsInfo.index,newsInfo.typeNewsSource,null, jsonString);
+		} else {
+			System.out.println("news crawler started......");
+			List<String> todayNewsLinks = page.getHtml()
+					.xpath("/html/body/div[@class='contentCon wrap clear']//div[@id='to_zx'")
+					.links().regex(contentUrlPattern).all();
+			System.out.println(todayNewsLinks);
+			page.addTargetRequests(todayNewsLinks);
+		}
+
+	}
+
+	@Override
+	public Site getSite() {
+		return site;
+	}
+}
